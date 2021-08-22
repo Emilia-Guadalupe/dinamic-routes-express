6ecfde4b6594255a133f0a6f65e59549f5d034ae
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-22 19:09:12 -0300

    Trying some dinamic routes

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+const users = [];
+let counter = 1;
+
+router.post('/user', (req, res) => {
+
+  const {body} = req;
+  const {name} = body;
+
+  if(body && name) {
+
+    const data = {
+      id: counter,
+      name: name,
+    };
+    counter = counter + 1;
+
+    users.push(data);
+
+    return res.sendStatus(201);
+
+  } else {
+    return res.sendStatus(400);
+  }
+});
+
+router.get('/user', (req, res) => {
+  return res.json(users);
+})
+
+router.get('/user/:id', (req, res) => {
+  const { params } = req;
+
+  const user = users.find((user) => {
+    return user.id == params.id;
+  })
+
+  if(user) {
+    return res.json(user);
+  } else {
+    res.sendStatus(404)
+  };
+})
+
+module.exports = router;
